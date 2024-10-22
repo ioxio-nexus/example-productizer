83abf822f4bc972f3f102470c1b41e08405e1361
Janne Enberg <janne.enberg@lietu.net>
2024-10-22 10:40:14 +0300

    Update api_tokens.py from example-app

--- app/api_tokens.py
+++ app/api_tokens.py
@@ -87,7 +87,40 @@ async def determine_jwks_url(dataspace_base_domain: str) -> str:
         URL.build(
             scheme="https",
             host=dataspace_base_domain,
-            path="/.well-known/dataspace/dataspace-configuration.json",
+            path="/.well-known/dataspace/dadef make_dsi(dataspace_base_domain: str, definition_path: str, source: str) -> str:
+    """
+    Construct a Data Source Identifier from the information on the request and token
+
+    DSIs are URIs in the format: dpp://<source>@<dataspace_base_domain>/<data_definition>
+    """
+
+    # This function somewhat counterintuitively expects / -prefixed paths, so we'll just fix it for you if needed
+    if not definition_path.startswith("/"):
+        definition_path = f"/{definition_path}"
+
+    # Data sources are identified on dataspaces as <group> or <group>:<variant>, which on the DSI URI correspond to a
+    # "user" and an optional "password"
+    user, _, password = source.partition(":")  # Extract group:source to URL properties
+
+    if not password:
+        # Passing the value None will not append it to the URL, whereas an empty string would be
+        password = None
+
+    # Use a URL builder to build the URI correctly
+    #
+    # Our expectation is that all the values are suitable for doing this with just string concatenation, however for
+    # future proofing we want to do this a bit more carefully here.
+
+    dsi_url = URL.build(
+        scheme="dpp",
+        host=dataspace_base_domain,
+        path=definition_path,
+        user=user,
+        password=password,
+    )
+
+    return str(dsi_url)
+taspace-configuration.json",
         )
     )
 
@@ -189,6 +222,10 @@ def make_dsi(dataspace_base_domain: str, definition_path: str, source: str) -> s
 
     DSIs are URIs in the format: dpp://<source>@<dataspace_base_domain>/<data_definition>
     """
+
+    # This function somewhat counterintuitively expects / -prefixed paths, so we'll just fix it for you if needed
+    if not definition_path.startswith("/"):
+        definition_path = f"/{definition_path}"
 
     # Data sources are identified on dataspaces as <group> or <group>:<variant>, which on the DSI URI correspond to a
     # "user" and an optional "password"
